b06b12a8c8d1f577d3145cf0e47719b2f98a3b65
Stew Esho <StewardEsho@gmail.com>
2016-06-17 10:57:19 -0400

    Added crosshairs

---
+++ Assets/Scripts/GUICrosshair.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUICrosshair : MonoBehaviour {
+
+	public Texture2D crosshairImage;
+	public int textureWidth = 32;
+	public int textureHeight = 32;
+
+	void OnGUI()
+	{
+    float xMin = (Screen.width / 2) - (textureWidth / 2);
+    float yMin = (Screen.height / 2) - (textureHeight / 2);
+    GUI.DrawTexture(new Rect(xMin, yMin, textureWidth, textureHeight), crosshairImage);
+	}
+}
