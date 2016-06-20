4aa9bcc8cd68bfbd305c600cbf33f23139e0046e
Stew Esho <StewardEsho@gmail.com>
2016-06-20 11:10:40 -0400

    Added rudimentary hit scan shooting.
    CUrrently, no damage is being done. All that happens is the console will print the tag of the object being aimed at.

---
+++ Assets/Scripts/RaycastShooting.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class RaycastShooting : MonoBehaviour {
+	void Update(){
+		if(Input.GetButtonDown("Fire1")){
+			RaycastHit hit;
+			Ray bulletRay = new Ray(transform.position, transform.forward);
+			if(Physics.Raycast(bulletRay, out hit)){
+				Debug.Log(hit.collider.tag);
+				Debug.DrawLine (transform.position, hit.point, Color.cyan);
+			}
+		}
+	}
+}
