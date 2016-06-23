7af13162a036a005a1ae65e4f36572878bdeb547
Stew Esho <StewardEsho@gmail.com>
2016-06-23 10:43:55 -0400

    Attempted to fix raycasting passing through enemies

--- Assets/Scripts/RaycastShooting.cs
+++ Assets/Scripts/RaycastShooting.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class RaycastShooting : MonoBehaviour {
 
-	void Update(){
+	void FixedUpdate(){
+		RaycastHit hit;
+		Ray bulletRay = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+
 		if(Input.GetButtonDown("Fire1")){
-			RaycastHit hit;
-			Ray bulletRay = new Ray(transform.position, transform.forward);
 			if(Physics.Raycast(bulletRay, out hit)){
 				Debug.Log(hit.collider.tag);
-				Debug.DrawLine (transform.position, hit.point, Color.red);
 				if(hit.collider.tag == "Enemy"){
 					hit.collider.gameObject.SetActive(false);
 					Debug.Log("Deadzo!");
