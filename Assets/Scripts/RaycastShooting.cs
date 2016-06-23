599deff27aa74b75e6c36e8b5c11b81ae3997092
Stew Esho <StewardEsho@gmail.com>
2016-06-23 12:12:44 -0400

    Used unity tutorial to revamp previous attempt at shooting.

--- Assets/Scripts/RaycastShooting.cs
+++ Assets/Scripts/RaycastShooting.cs
@@ -3,18 +3,52 @@ using System.Collections;
 
 public class RaycastShooting : MonoBehaviour {
 
-	void FixedUpdate(){
-		RaycastHit hit;
-		Ray bulletRay = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+	public int bulletDamage = 20;
+	public float timeBetweenBullets = 0.15f;
+	public float bulletRange = 200f;
 
-		if(Input.GetButtonDown("Fire1")){
-			if(Physics.Raycast(bulletRay, out hit)){
-				Debug.Log(hit.collider.tag);
-				if(hit.collider.tag == "Enemy"){
-					hit.collider.gameObject.SetActive(false);
-					Debug.Log("Deadzo!");
-				}
-			}
+	Ray bulletRay;
+	RaycastHit bulletHit;
+	int shootableMask;
+	LineRenderer gunLine;
+	float effectsDisplayTime = 0.4f;
+	float timer;
+
+	void Awake(){
+		shootableMask = 8;
+		gunLine = transform.Find("Tesla").gameObject.GetComponent<LineRenderer>();
+	}
+
+	void Update(){
+		timer += Time.deltaTime;
+
+		if(Input.GetButton("Fire1") && timer >= timeBetweenBullets){
+			Shoot();
+		}
+
+		if(timer >= timeBetweenBullets * effectsDisplayTime){
+			// DisableEffects();
+		}
+	}
+
+	public void DisableEffects(){
+		gunLine.enabled = false;
+	}
+
+	void Shoot(){
+		timer = 0.0F;
+
+		gunLine.enabled = true;
+		gunLine.SetPosition(0, transform.position);
+
+		bulletRay = new Ray(transform.position, transform.forward);
+
+		if(Physics.Raycast(bulletRay, out bulletHit, bulletRange, shootableMask)){
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = bulletHit.point;
+			gunLine.SetPosition(1, bulletHit.point);
+		} else {
+			gunLine.SetPosition(1, bulletRay.origin + bulletRay.direction * bulletRange);
 		}
 	}
 }
